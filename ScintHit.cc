f733d6dfa4597ab6990e8f5756c8ea2372d08a01
pewlt <98036882+pewlt@users.noreply.github.com>
2023-04-22 17:27:00 +0200

    Update ScintHit.cc

--- ScintHit.cc
+++ ScintHit.cc
@@ -9,26 +9,28 @@
 
 namespace B1
 
-{
+//{
 
 //G4ThreadLocal G4Allocator<CalorHit>* CalorHitAllocator = nullptr;
 
 
-G4bool ScintHit::operator==(const ScintHit& right) const
-{
-  return ( this == &right ) ? true : false;
-}
+//G4bool ScintHit::operator==(const ScintHit& right) const
+//{
+  //return ( this == &right ) ? true : false;
+//}
 
 
 void ScintHit::Print()
 {
   G4cout
      << "Edep: "
-     << std::setw(7) << G4BestUnit(fEdep,"Energy")
-     << ”hitTime: ”
-     << std::setw(7) << G4BestUnit(hitTime,”hitTime")
-     << " track length: "
-     << std::setw(7) << G4BestUnit( fTrackLength,"Length")
+     << std::setw(7) << G4BestUnit(fEdep,"Energy") //Coompréhension std::setw(7)
+     << "hitTime:"
+     << std::setw(7) << G4BestUnit(hitTime,"hitTime")
+     << " Numero_channel: "
+     << std::setw(7) << G4BestUnit( channelNbr,"channel number")
+     << "Numero du panel"
+     << std::setw(7) << G4BestUnit(panelNbr,"Panel number")
      << G4endl;
 }
 
