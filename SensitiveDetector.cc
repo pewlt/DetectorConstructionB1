c958e8e71162c882865b56dcf869988cbf848279
pewlt <98036882+pewlt@users.noreply.github.com>
2023-04-22 20:51:45 +0200

    Create SensitiveDetector.cc

---
+++ SensitiveDetector.cc
@@ -0,0 +1,97 @@
+#include "SensitiveDetector.hh"
+#include "G4HCofThisEvent.hh"
+#include "G4Step.hh"
+#include "G4ThreeVector.hh"
+#include "G4SDManager.hh"
+#include "G4ios.hh"
+
+namespace B1
+  
+{
+
+
+ScintillatorSD::ScintillatorSD(const G4String& name,
+                             const G4String& hitsCollectionName,
+                             G4int nofCells) 
+ : G4VSensitiveDetector(name),
+   fNofCells(nofCells)
+{
+  collectionName.insert(hitsCollectionName);
+}
+
+
+void CalorimeterSD::Initialize(G4HCofThisEvent* hce)
+{
+  // Create hits collection
+  fHitsCollection
+    = new CalorHitsCollection(SensitiveDetectorName, collectionName[0]);
+
+  // Add this collection in hce
+  auto hcID
+    = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
+  hce->AddHitsCollection( hcID, fHitsCollection );
+
+  // Create hits
+  // fNofCells for cells + one more for total sums
+  for (G4int i=0; i<fNofCells+1; i++ ) {
+    fHitsCollection->insert(new CalorHit());
+  }
+}
+
+  
+G4bool CalorimeterSD::ProcessHits(G4Step* step,
+                                     G4TouchableHistory*)
+{
+  // energy deposit
+  auto edep = step->GetTotalEnergyDeposit();
+
+  // step length
+  G4double stepLength = 0.;
+  if ( step->GetTrack()->GetDefinition()->GetPDGCharge() != 0. ) {
+    stepLength = step->GetStepLength();
+  }
+
+  if ( edep==0. && stepLength == 0. ) return false;
+
+  auto touchable = (step->GetPreStepPoint()->GetTouchable());
+
+  // Get calorimeter cell id
+  auto layerNumber = touchable->GetReplicaNumber(1);
+
+  // Get hit accounting data for this cell
+  auto hit = (*fHitsCollection)[layerNumber];
+  if ( ! hit ) {
+    G4ExceptionDescription msg;
+    msg << "Cannot access hit " << layerNumber;
+    G4Exception("CalorimeterSD::ProcessHits()",
+      "MyCode0004", FatalException, msg);
+  }
+
+  // Get hit for total accounting
+  auto hitTotal
+    = (*fHitsCollection)[fHitsCollection->entries()-1];
+
+  // Add values
+  hit->Add(edep, stepLength);
+  hitTotal->Add(edep, stepLength);
+
+  return true;
+}
+
+
+void CalorimeterSD::EndOfEvent(G4HCofThisEvent*)
+{
+  if ( verboseLevel>1 ) {
+     auto nofHits = fHitsCollection->entries();
+     G4cout
+       << G4endl
+       << "-------->Hits Collection: in this event they are " << nofHits
+       << " hits in the tracker chambers: " << G4endl;
+     for ( std::size_t i=0; i<nofHits; ++i ) (*fHitsCollection)[i]->Print();
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+}
+
