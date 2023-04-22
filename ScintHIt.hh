bc35dc98dfdf1397583455183792fc03e4fe2346
pewlt <98036882+pewlt@users.noreply.github.com>
2023-04-22 15:38:13 +0200

    Update ScintHIt.hh

--- ScintHIt.hh
+++ ScintHIt.hh
@@ -1,35 +1,42 @@
-#include "CalorHit.hh"
-#include "G4UnitsTable.hh"
-#include "G4VVisManager.hh"
-#include "G4Circle.hh"
-#include "G4Colour.hh"
-#include "G4VisAttributes.hh"
+#ifndef SLABHIT_HH
+#define SLABHIT_HH
 
-#include <iomanip>
 
-namespace B1
+#include "G4VHit.hh"
+#include "G4THitsCollection.hh"
+#include "G4Allocator.hh"
+#include "G4ThreeVector.hh"
+#include "G4Threading.hh"
+
+
+class SlabHit
 {
+    public:
+        SlabHit() = default;
+        SlabHit(const CalorHit&) = default;
+        ~SlabHit() override = default;
 
-G4ThreadLocal G4Allocator<CalorHit>* CalorHitAllocator = nullptr;
+        
+        G4double GetEdep() {return fEdep;};
+        void SetEdep(double edep) {fEdep = edep;};
+        
+        G4double GetHitTime() {return hitTime;};
+        void SetHitTime(double hit_time) {hitTime = hit_time;};
+        
+        G4int GetChannelNbr() {return channelNbr;};
+        void SetChannelNbr(G4int channel_number) {channelNbr = channel_number;};
+        
+        G4int GetPanelNbr() {return panelNbr;};
+        void SetPanelNbr(G4int panel_number) {panelNbr = panel_number;};
+    
+    private:
+        G4double fEdep; // Energy deposition
+        G4double hitTime; 
+        G4int channelNbr;
+        G4int panelNbr;
+    
+};
 
+using SlabHitsCollection = G4THitsCollection<SlabHit>;
 
-G4bool ScintHit::operator==(const ScintHit& right) const
-{
-  return ( this == &right ) ? true : false;
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-void ScintHit::Print()
-{
-  G4cout
-     << "Edep: "
-     << std::setw(7) << G4BestUnit(fEdep,"Energy")
-     << " track length: "
-     << std::setw(7) << G4BestUnit( fTrackLength,"Length")
-     << G4endl;
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-}
+#endif
