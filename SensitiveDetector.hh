372a4d75b51ff812674139507084f3c9f23ef902
pewlt <98036882+pewlt@users.noreply.github.com>
2023-05-31 17:23:58 +0200

    Update SensitiveDetector.hh

--- SensitiveDetector.hh
+++ SensitiveDetector.hh
@@ -1,9 +1,38 @@
-#ifndef B1DetectorConstruction_h //A CHANGER ?? Je ne sais pas quoi mettre
-#define B1DetectorConstruction_h 1
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1bis/src/SensitiveDetector.hh
+/// \brief Definition of the B1::SensitiveDetector class
+
+#ifndef SensitiveDetector_h
+#define SensitiveDetector_h 1
 
 #include "G4VSensitiveDetector.hh"
 
-#include "ScintHit.hh"
+#include "SlabHit.hh"
 
 #include <vector>
 
@@ -13,34 +42,44 @@ class G4HCofThisEvent;
 namespace B1
 {
 
-/// Scitillator sensitive detector class
+/// Scintillator sensitive detector class
 ///
-/// In Initialize(), it creates one hit for each slab of the scintillator
-/// hit for accounting the total quantities in all layers.
+/// In Initialize(), it creates one hit for each slab and one more
+/// hit for accounting the total quantities in all p.
 ///
 /// The values are accounted in hits in ProcessHits() function which is called
 /// by Geant4 kernel at each step.
 
-class ScintillatorSD : public G4VSensitiveDetector
+class SensitiveDetector : public G4VSensitiveDetector
 {
   public:
-    ScintillatorSD(const G4String& name,
-                  const G4String& hitsCollectionName,
-                  G4int nofCells);
-    ~ScintillatorSD() override = default;
+    SensitiveDetector(const G4String& name);
+    ~SensitiveDetector() override = default;
 
     // methods from base class
     void   Initialize(G4HCofThisEvent* hitCollection) override;
     G4bool ProcessHits(G4Step* step, G4TouchableHistory* history) override;
     void   EndOfEvent(G4HCofThisEvent* hitCollection) override;
+    void Add(G4double de, G4double dl);
 
   private:
     SlabHitsCollection* fHitsCollection = nullptr;
-    G4int fNofCells = 0;
+    G4int fNofCells = 10;
+    G4double fEdep = 0;
+    G4double fHitTime = 0;
+//    G4int fNofCellsPannel = 0;
 };
 
+inline void SensitiveDetector::Add(G4double de, G4double dl) {
+  fEdep += de;
+  fHitTime += dl;
 }
 
 
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #endif
+
+
