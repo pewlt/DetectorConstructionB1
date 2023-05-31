8adaab9f0b937327a8f99a968fa5ea8451221ed8
pewlt <98036882+pewlt@users.noreply.github.com>
2023-05-31 17:24:27 +0200

    Update ScintHit.hh

--- ScintHit.hh
+++ ScintHit.hh
@@ -1,5 +1,13 @@
-#ifndef SLABHIT_HH
-#define SLABHIT_HH
+
+
+
+/// \file B1bis/src/SlabHit.hh
+/// \brief Implementation of the B1::SlabHit class
+
+
+
+#ifndef SlabHit_h
+#define SlabHit_h
 
 
 #include "G4VHit.hh"
@@ -7,52 +15,67 @@
 #include "G4Allocator.hh"
 #include "G4ThreeVector.hh"
 #include "G4Threading.hh"
+using namespace std;
 
-
-class SlabHit
+namespace B1
+{
+class SlabHit : public G4VHit
 {
     public:
         SlabHit() = default;
-        SlabHit(const CalorHit&) = default;
+        SlabHit(const SlabHit&) = default;
         ~SlabHit() override = default;
-        
-        //Operators
-        ScintHit& operator = (const ScintHit&) = default;
-        G4bool operator == (const ScintHit&) const;
-        // A quoi vont servir les operators ? 
-        
-        inline void* operator new(size_t); //What is size_t ?
-        inline void operator delete(void*);
-        
-        //Methods from base class
-        void Draw()  override{}
-        void Print() override;
 
-        //Methods to handle data
-        void Add(G4double de, G4double dl);
-
-        //Get methods 
+        
         G4double GetEdep() {return fEdep;};
-        void SetEdep(double edep) {fEdep = edep;};
+        void SetEdep(double edep) {fEdep += edep;};
         
         G4double GetHitTime() {return hitTime;};
-        void SetHitTime(double hit_time) {hitTime = hit_time;};
+        void SetHitTime(double hit_time) {hitTime += hit_time;};
         
-        G4int GetChannelNbr() {return channelNbr;};
-        void SetChannelNbr(G4int channel_number) {channelNbr = channel_number;};
+        vector<G4int> GetSlabVector() {return SlabVector;};
+        void SetSlabVector(G4int Slab_number) {SlabVector.push_back(Slab_number);};
         
         G4int GetPanelNbr() {return panelNbr;};
         void SetPanelNbr(G4int panel_number) {panelNbr = panel_number;};
+        
+        
+        // Operators -> Utiles ? 
+        
+        G4double GetEdepTot() {return fEdepTot;};
+//        void SetEdepTot(double edepTot) {fEdepTot = edepTot;};
+        
+        G4double GetHitTimeTot() {return hitTimeTot;};
+//        void SetHitTimeTot(double hit_timeTot) {hitTimeTot = hit_timeTot;};
+        
+        void Add(G4double de, G4double dt);
+        
+            // methods from base class
+        void Draw()  override{}
+        void Print() override;
+
+        
+        
     
     private:
-        G4double fEdep; // Energy deposition
-        G4double hitTime; 
-        G4int channelNbr;
-        G4int panelNbr;
+        
+        G4double fEdep = 0; // Energy deposited
+        G4double hitTime = 0; 
+        vector<G4int> SlabVector;
+        G4int panelNbr = 0;
+        G4double fEdepTot = 0; // Energy deposition totale
+        G4double hitTimeTot = 0;
     
 };
 
+
 using SlabHitsCollection = G4THitsCollection<SlabHit>;
 
+
+inline void SlabHit::Add(G4double de, G4double dt) {
+  fEdepTot += de;
+  hitTimeTot += dt;
+}
+}
+
 #endif
-//+++ tout ce qui concerne CalorHitAllocator -> Recherche/comprendre si on en a besoin 
