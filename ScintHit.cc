2059fa5dcedb4b223f192fbd5c322039ac7f12d5
pewlt <98036882+pewlt@users.noreply.github.com>
2023-05-31 17:19:48 +0200

    SlabHit.cc

--- ScintHit.cc
+++ ScintHit.cc
@@ -1,4 +1,33 @@
-#include "ScintHit.hh"
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1bis/src/SlabHit.cc
+/// \brief Implementation of the B1::SlabHit class
+
+#include "SlabHit.hh" 
 #include "G4UnitsTable.hh"
 #include "G4VVisManager.hh"
 #include "G4Circle.hh"
@@ -8,32 +37,29 @@
 #include <iomanip>
 
 namespace B1
+{
+/*
+G4ThreadLocal G4Allocator<SlabHit>* SlabHitAllocator = nullptr;
 
-//{
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-//G4ThreadLocal G4Allocator<CalorHit>* CalorHitAllocator = nullptr;
+G4bool SlabHit::operator==(const SlabHit& right) const
+{
+  return ( this == &right ) ? true : false;
+}
+*/
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-
-//G4bool ScintHit::operator==(const ScintHit& right) const
-//{
-  //return ( this == &right ) ? true : false;
-//}
-
-
-void ScintHit::Print()
+void SlabHit::Print()
 {
   G4cout
      << "Edep: "
-     << std::setw(7) << G4BestUnit(fEdep,"Energy") //Coompréhension std::setw(7)
-     << "hitTime:"
-     << std::setw(7) << G4BestUnit(hitTime,"hitTime")
-     << " Numero_channel: "
-     << std::setw(7) << G4BestUnit( channelNbr,"channel number")
-     << "Numero du panel"
-     << std::setw(7) << G4BestUnit(panelNbr,"Panel number")
+     << std::setw(7) << G4BestUnit(fEdep,"Energy")
+     << " track length: "
+     << std::setw(7) << G4BestUnit( hitTime,"Time")
      << G4endl;
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 }
-
