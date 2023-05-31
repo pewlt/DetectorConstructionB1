6cdc48b422173b30f9e4a5ae7b38f10c4be47661
pewlt <98036882+pewlt@users.noreply.github.com>
2023-05-31 17:21:24 +0200

    Update DetectorConstruction.hh

--- DetectorConstruction.hh
+++ DetectorConstruction.hh
@@ -1,5 +1,34 @@
-#ifndef B1DetectorConstruction_h
-#define B1DetectorConstruction_h 1
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1bis/include/DetectorConstruction.hh
+/// \brief Definition of the B1::DetectorConstruction class
+
+#ifndef DETECTORCONSTRUCTION_HH
+#define DETECTORCONSTRUCTION_HH 1
 
 #include "G4VUserDetectorConstruction.hh"
 #include "globals.hh"
@@ -17,19 +46,20 @@ class DetectorConstruction : public G4VUserDetectorConstruction
   public:
     DetectorConstruction() = default;
     ~DetectorConstruction() override = default;
+    virtual void ConstructSD();
+    public: 
+    void DeleteMessenger();
+    virtual G4VPhysicalVolume* Construct();
+    G4LogicalVolume* GetScoringVolume() const { return fLogicScintillator; }//logic_lead
+  
 
-    G4VPhysicalVolume* Construct() override;
-    void MyDetectorConstruction::ConstructSDandField() override;
-  private:
-    //Methods
-    G4VPhysicalVolume* DefineVolumes();
-
-    G4bool fCheckOverlaps = true; // option to activate checking of volumes overlaps
-    G4int  fNofLayers = -1;
-
+  protected:
+    G4LogicalVolume* fLogicScintillator = nullptr;
+    
 };
 
 }
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #endif
