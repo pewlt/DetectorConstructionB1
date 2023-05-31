7f928618c0a4001c1ddb2b604ea9de8d105394cc
pewlt <98036882+pewlt@users.noreply.github.com>
2023-05-31 17:18:11 +0200

    Create RunAction.cc

---
+++ RunAction.cc
@@ -0,0 +1,267 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1bis/src/RunAction.cc
+/// \brief Implementation of the B1::RunAction class
+
+#include "RunAction.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "DetectorConstruction.hh"
+
+
+#include "G4RunManager.hh"
+#include "G4Run.hh"
+#include "G4AccumulableManager.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4LogicalVolume.hh"
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+
+
+//Ajout
+#include "G4AnalysisManager.hh"
+
+namespace B1
+{
+RunAction::RunAction()
+{
+  // set printing event number per each event
+  G4RunManager::GetRunManager()->SetPrintProgress(1);
+
+  // Create analysis manager
+  // The choice of the output format is done via the specified
+  // file extension.
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // Create directories
+  //analysisManager->SetHistoDirectoryName("histograms");
+  //analysisManager->SetNtupleDirectoryName("ntuple");
+  analysisManager->SetVerboseLevel(1);
+  analysisManager->SetNtupleMerging(true);
+    // Note: merging ntuples is available only with Root output
+
+  // Book histograms, ntuple
+  //
+
+  // Creating histograms
+  analysisManager->CreateH1("Edeposited","Edep in absorber", 100, 0., 800*MeV);
+  analysisManager->CreateH1("Egap","Edep in gap", 100, 0., 100*MeV);
+  analysisManager->CreateH1("LPanel","trackL in Panel", 100, 0., 1*m); //Pe panel en slab ? 
+  analysisManager->CreateH1("Lgap","trackL in gap", 100, 0., 50*cm);
+
+  // Creating ntuple
+  //
+  analysisManager->CreateNtuple("B1", "Edeposited and TrackL"); //A changer TrackL en hit time 
+  analysisManager->CreateNtupleDColumn("Edep");
+  analysisManager->CreateNtupleDColumn("Egap");
+  analysisManager->CreateNtupleDColumn("Labs"); 
+  analysisManager->CreateNtupleDColumn("Lgap");
+  analysisManager->FinishNtuple();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::BeginOfRunAction(const G4Run* /*run*/)
+{
+  //inform the runManager to save random number seed
+  //G4RunManager::GetRunManager()->SetRandomNumberStore(true);
+ G4cout << ">>>>>>>>>>>>>>>>>DEBUG 1" << G4endl;
+  // Get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // Open an output file
+  //
+  G4String fileName = "B1.root";
+  // Other supported output types:
+  // G4String fileName = "B4.csv";
+  // G4String fileName = "B4.hdf5";
+  // G4String fileName = "B4.xml";
+  analysisManager->OpenFile(fileName);
+  G4cout << "Using " << analysisManager->GetType() << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::EndOfRunAction(const G4Run* /*run*/)
+{
+  // print histogram statistics
+  //
+  auto analysisManager = G4AnalysisManager::Instance();
+  if ( analysisManager->GetH1(1) ) {
+    G4cout << G4endl << " ----> print histograms statistic ";
+    if(isMaster) {
+      G4cout << "for the entire run " << G4endl << G4endl;
+    }
+    else {
+      G4cout << "for the local thread " << G4endl << G4endl;
+    }
+
+    G4cout << " Edeposited : mean = "
+       << G4BestUnit(analysisManager->GetH1(0)->mean(), "Energy")
+       << " rms = "
+       << G4BestUnit(analysisManager->GetH1(0)->rms(),  "Energy") << G4endl;
+
+    G4cout << " EGap : mean = "
+       << G4BestUnit(analysisManager->GetH1(1)->mean(), "Energy")
+       << " rms = "
+       << G4BestUnit(analysisManager->GetH1(1)->rms(),  "Energy") << G4endl;
+
+    G4cout << " LPanel : mean = "
+      << G4BestUnit(analysisManager->GetH1(2)->mean(), "Length")
+      << " rms = "
+      << G4BestUnit(analysisManager->GetH1(2)->rms(),  "Length") << G4endl;
+
+    G4cout << " LGap : mean = "
+      << G4BestUnit(analysisManager->GetH1(3)->mean(), "Length")
+      << " rms = "
+      << G4BestUnit(analysisManager->GetH1(3)->rms(),  "Length") << G4endl;
+  }
+
+  // save histograms & ntuple
+  //
+  analysisManager->Write();
+  analysisManager->CloseFile();
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+/*
+RunAction::RunAction()
+{
+  // add new units for dose
+  //
+  const G4double milligray = 1.e-3*gray;
+  const G4double microgray = 1.e-6*gray;
+  const G4double nanogray  = 1.e-9*gray;
+  const G4double picogray  = 1.e-12*gray;
+
+  new G4UnitDefinition("milligray", "milliGy" , "Dose", milligray);
+  new G4UnitDefinition("microgray", "microGy" , "Dose", microgray);
+  new G4UnitDefinition("nanogray" , "nanoGy"  , "Dose", nanogray);
+  new G4UnitDefinition("picogray" , "picoGy"  , "Dose", picogray);
+
+  // Register accumulable to the accumulable manager
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->RegisterAccumulable(fEdep);
+  accumulableManager->RegisterAccumulable(fEdep2);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::BeginOfRunAction(const G4Run*)
+{
+  // inform the runManager to save random number seed
+  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+
+  // reset accumulables to their initial values
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->Reset();
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::EndOfRunAction(const G4Run* run)
+{
+  G4int nofEvents = run->GetNumberOfEvent();
+  if (nofEvents == 0) return;
+
+  // Merge accumulables
+  G4AccumulableManager* accumulableManager = G4AccumulableManager::Instance();
+  accumulableManager->Merge();
+
+  // Compute dose = total energy deposit in a run and its variance
+  //
+  G4double edep  = fEdep.GetValue();
+  G4double edep2 = fEdep2.GetValue();
+
+  G4double rms = edep2 - edep*edep/nofEvents;
+  if (rms > 0.) rms = std::sqrt(rms); else rms = 0.;
+
+  const auto detConstruction = static_cast<const DetectorConstruction*>(
+    G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+  G4double mass = detConstruction->GetScoringVolume()->GetMass();
+  G4double dose = edep/mass;
+  G4double rmsDose = rms/mass;
+
+  // Run conditions
+  //  note: There is no primary generator action object for "master"
+  //        run manager for multi-threaded mode.
+  const auto generatorAction = static_cast<const PrimaryGeneratorAction*>(
+    G4RunManager::GetRunManager()->GetUserPrimaryGeneratorAction());
+  G4String runCondition;
+  if (generatorAction)
+  {
+    const G4ParticleGun* particleGun = generatorAction->GetParticleGun();
+    runCondition += particleGun->GetParticleDefinition()->GetParticleName();
+    runCondition += " of ";
+    G4double particleEnergy = particleGun->GetParticleEnergy();
+    runCondition += G4BestUnit(particleEnergy,"Energy");
+  }
+
+  // Print
+  //
+  if (IsMaster()) {
+    G4cout
+     << G4endl
+     << "--------------------End of Global Run-----------------------";
+  }
+  else {
+    G4cout
+     << G4endl
+     << "--------------------End of Local Run------------------------";
+  }
+
+  G4cout
+     << G4endl
+     << " The run consists of " << nofEvents << " "<< runCondition
+     << G4endl
+     << " Cumulated dose per run, in scoring volume : "
+     << G4BestUnit(dose,"Dose") << " rms = " << G4BestUnit(rmsDose,"Dose")
+     << G4endl
+     << "------------------------------------------------------------"
+     << G4endl
+     << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::AddEdep(G4double edep)
+{
+  fEdep  += edep;
+  fEdep2 += edep*edep;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+*/
+}
